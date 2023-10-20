fe58492b1a01017a55ca92bcae4efe4fcb699cd0
Alonso887 <138240277+Alonso887@users.noreply.github.com>
2023-10-20 17:47:33 -0600

    added the box proyect, finished the box  design

---
+++ Pyfiles/#9-Box.py
@@ -0,0 +1,11 @@
+import os, time, random
+box_design = "  ______\n /     /|\n/_____/ |\n|     | |\n| BOX | /\n|_____|/"
+
+def s(n):
+    time.sleep(n)
+def c():
+    os.system("cls")
+
+def print_boxes():#bd means boxdata
+    None
+print(box_design)
