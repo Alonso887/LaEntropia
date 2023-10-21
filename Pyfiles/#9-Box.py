904d92f1b16f27e57ffdde8039db53ca3b1b956c
Alonso887 <138240277+Alonso887@users.noreply.github.com>
2023-10-20 18:25:49 -0600

    Now i added more boxes designs cause i'm stupid

--- Pyfiles/#9-Box.py
+++ Pyfiles/#9-Box.py
@@ -1,6 +1,8 @@
 import os, time, random
-box_design = "  ______\n /     /|\n/_____/ |\n|     | |\n| BOX | /\n|_____|/"
-
+box_designs = ["  ______ \n /     /|\n/_____/ |\n|     | |\n| BOX | /\n|_____|/ ",
+              "  ┌----┐\n  |    | \n  |____| \n /     /|\n/_____/ |\n|     | |\n| BOX | /\n|_____|/ ",
+              "┌──┐ ┌───┐ \n└ ┌-_-┐  ┘\n  |---|_ \n /|---|/|\n/_|___/ |\n|     | |\n| BOX | /\n|_____|/ "]
+#[0] is closed box, [1] is opne em´ty box and [2] is open carrot box
 def s(n):
     time.sleep(n)
 def c():
@@ -8,4 +10,4 @@ def c():
 
 def print_boxes():#bd means boxdata
     None
-print(box_design)
+print(box_designs[2])
