c81b9448f3d7cd5e1921b2dd17086d714ec7bb77
Alonso887 <138240277+Alonso887@users.noreply.github.com>
2023-11-02 19:12:55 -0600

    just messing around with tkinter

--- Pyfiles/Task/Excel_reader.py
+++ Pyfiles/Task/Excel_reader.py
@@ -1,9 +1,9 @@
-from openpyxl import Workbook
-from openpyxl import load_workbook
+from tkinter import *
+import tkinter as tk
+main_wind = tk.Tk()
+main_wind.geometry('700x500')
+real_deal = tk.Entry()
+real_deal.pack(side=TOP,padx=50,pady=30)
+main_wind.mainloop()
 
-wb = Workbook()
-wb = load_workbook(filename=r"C:\Users\aadri\Desktop\Escuela\Promedio calificaciones Alo.xlsx")
-worksheets = wb.sheetnames
-ws1 = wb.active
-ws1['A1'] = 8
-wb.save("Promedio_calificaciones_OMG.xlsx")
+
