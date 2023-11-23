8ddd618d714f98613f9eac5ca815ef1d6c19fba6
Alonso887 <138240277+Alonso887@users.noreply.github.com>
2023-11-22 20:26:02 -0600

    nthing

--- Pyfiles/Try.py
+++ Pyfiles/Try.py
@@ -1,1 +1,11 @@
+import tkinter as tk 
+from tkcalendar import Calendar
+
+root = tk.Tk()
+root.geometry("800x600")
+
+cal = Calendar(root,selectmode="day", year=2023, month=1, day=1)
+cal.pack(pady=10)
+
+root.mainloop()
 
